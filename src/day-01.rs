3d1384f793be1d36d8950a6e63061e76e24218c2
Brian McDonald <bmcdonald916@gmail.com>
2020-12-01 13:57:18 -0500

    Add a utils module for common functions
    This commit adds a utils module and moves the read_puzzle_input method
    to that module so it can be used in future days puzzles. Other common
    functions may be added there in the future as well.

--- src/day-01.rs
+++ src/day-01.rs
@@ -1,10 +1,7 @@
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+mod utils;
 
 fn main() {
-    let input_lines = read_puzzle_input();
+    let input_lines = utils::read_puzzle_input();
     let values: Vec<i32> = input_lines
         .iter()
         .map(|line| line.parse::<i32>().unwrap())
@@ -25,20 +22,4 @@ fn main() {
             }
         }
     }
-}
-
-fn read_puzzle_input() -> Vec<String> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("The input file must be provided as an argument.");
-    }
-    let input_path = Path::new(&args[1]);
-    let file = match File::open(&input_path) {
-        Err(why) => panic!("Unable to open file {}: {}", input_path.display(), why),
-        Ok(file) => file,
-    };
-    return io::BufReader::new(file)
-        .lines()
-        .map(|l| l.unwrap())
-        .collect();
 }
